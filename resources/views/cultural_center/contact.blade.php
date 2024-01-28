939e64c5dd9e109cbb51e8a7b94c6aa46c447dd6
AhmedMahmoud962 <am6944430@gmail.com>
2024-01-28 19:32:24 +0200

    edit page news-details and contact

--- resources/views/cultural_center/contact.blade.php
+++ resources/views/cultural_center/contact.blade.php
@@ -8,16 +8,14 @@
                 <div class="hadding-section pt-80 pb-80">
                     <div class="container">
                         <div class="row">
-                            <div class="col-xl-5 col-lg-5 col-md-12 align-items-center c-center mb-5">
+                            <div class="col-xl-5 col-lg-5 col-md-12 align-items-center c-center mb-3">
                                 <div class="hadding-text-area">
                                     <h2 class="black-c popin small">اتصل بنا</h2>
                                     <p>{!! $contact_us->description !!}</p>
                                 </div>
-
                             </div>
-                            <div class="col-xl-7 col-lg-7 col-md-12   align-items-center c-center">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3608.4153753930923!2d55.3133384!3d25.2566088!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f432dbd28cc83%3A0xa727e79897b9f8b5!2z2KfZhNmC2YbYtdmE2YrYqSDYp9mE2YPZiNmK2KrZitip!5e0!3m2!1sar!2s!4v1684771306030!5m2!1sar!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
+                            <div class="col-xl-7 col-lg-7 col-md-12 align-items-center c-center" >
+                                <iframe style="width:100%" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3608.4153753930923!2d55.3133384!3d25.2566088!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f432dbd28cc83%3A0xa727e79897b9f8b5!2z2KfZhNmC2YbYtdmE2YrYqSDYp9mE2YPZiNmK2KrZitip!5e0!3m2!1sar!2s!4v1684771306030!5m2!1sar!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                             </div>
                         </div>
                     </div>
--- resources/views/cultural_center/news-details.blade.php
+++ resources/views/cultural_center/news-details.blade.php
@@ -9,7 +9,7 @@
         <!-- news details -->
         <div class="service-details pb-80 pt-80">
             <div class="container">
-                <div class="tab-content col-xl-9 col-lg-9 col-md-8 col-sm-12 col-12">
+                <div class="tab-content col-xl-10 col-lg-10 col-md-8 col-sm-12 col-12" style="width:100%;margin:0 auto;">
                     <div class="tab-pane fade show active" id="Sales">
                         <div class="service-details-content">
                             <div class="service-d-img">
