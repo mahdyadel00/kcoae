9fc840596aa2c15e2a92381c794ec583c7393fa7
AhmedMahmoud962 <am6944430@gmail.com>
2024-02-09 21:36:02 +0200

    edit login , register

--- resources/views/client/register.blade.php
+++ resources/views/client/register.blade.php
@@ -30,81 +30,66 @@
                         <div class="comment-area mb-5">
                             <div class="comment-respond">
 
-                            <h2 style="color:#d1b686"> نسيت كلمة السر</h2>
+                                <h2 style="color:#d1b686">انشاء حساب جديد</h2>
                                 <div class="respons-box pt-4">
                                     <div class="form">
-                                    <form method="post" action="reset_code">
-                                        @csrf
-                                        <div class="row">
+                                        <form method="post" action="register">
+                                            @csrf
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label for="name">اسم المستخدم</label>
+                                                        <input id="name" class="form-control form-mane" required="" type="text" name="name"  placeholder="ادخل اسم المستخدم" style="padding: 10px !important;">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label for="name">الرقم الوطني</label>
+                                                        <input id="name" class="form-control form-mane" required="" type="text" name="ID_number" placeholder="ادخل الرقم الوطني" style="padding: 10px !important;">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label for="email">البريد الألكتروني</label>
+                                                        <input id="email" class="form-control form-email" required="" type="email" name="email" placeholder="ادخل البريد الألكتروني" style="padding: 10px !important;">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label for="name">رقم الهاتف</label>
+                                                        <input id="name" class="form-control form-mane" required="" type="text" name="mobile" placeholder="ادخل رقم الهاتق" style="padding: 10px !important;">
+                                                    </div>
+                                                </div>
+                                            
                                             <div class="col-md-12">
                                                 <div class="form-group">
-                                                    <label for="name">البريد الالكتروني</label>
-                                                    <input id="name" placeholder="ادخل البريد الالكتروني" class="form-control form-mane" required="" type="text" name="email" style="padding: 10px !important;">
+                                                    <label for="name">كلمة السر</label>
+                                                    <input id="name" placeholder="ادخل كلمه المرور" class="form-control form-mane" title="كلمة المرور يجب ان تتكون من ثمانية محارف على الأقل و تحوي محرف صغير و محرف كبير و رمز " class="form-control form-mane" required="" type="password" name="password" style="padding: 10px !important;">
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="buttons pt-4">
-                                            <button type="submit" class="btn btn-button btn-button-1 blue-bg">ارسال رمز التحقق الى البريد الالكتروني</button>
-                                        </div>
-                                    </form>
+
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label for="name">تأكيد كلمة السر</label>
+                                                    <input id="name" placeholder="تأكيد كلمه المرور" class="form-control form-mane" required="" type="password" name="password_confirmation" style="padding: 10px !important;">
+                                                </div>
+                                            </div>
+                                            <div class="buttons pt-4">
+                                                <button type="submit" class="btn btn-button btn-button-1 blue-bg">انشاء حساب </button>
+                                            </div>
+                                            </div>
+                                            <div class="pt-4" style="display: flex;justify-content: space-between;flex-wrap: wrap;">
+                                                <a href="#"  class="btn" style="color:">  دخول للحساب </a>
+                                                <a href="#"  class="btn">  ارسال رمز التحقق الى البريد الالكتروني </a>
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
                             <!-- END COMMENT RESPOND -->
                         </div>
                     </div>
-                    <!-- <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
-                        <div class="contact-widget mb-5">
-                            <h5 class="contact-widget-title no-margin"><span> الدخول للحساب</span></h5>
-                            <div class="textwidget">
-                                <div class="respons-box">
-                                    <div class="form">
-                                        <form method="post" action="client_login">
-                                            @csrf
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="name">الرقم الوطني</label>
-                                                        <input id="name" class="form-control form-mane" required="" type="text" name="ID_number">
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="name">كلمة السر</label>
-                                                        <input id="name" class="form-control form-mane" required="" type="password" name="password">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="buttons pt-4">
-                                                <button type="submit" class="btn btn-button btn-button-1 blue-bg">الدخول </button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="contact-widget">
-                            <h5 class="contact-widget-title no-margin"><span>نسيت كلمة السر </span></h5>
-                            <div class="textwidget">
-                                <div class="form">
-                                    <form method="post" action="reset_code">
-                                        @csrf
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="form-group">
-                                                    <label for="name">البريد الالكتروني</label>
-                                                    <input id="name" class="form-control form-mane" required="" type="text" name="email">
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="buttons pt-4">
-                                            <button type="submit" class="btn btn-button btn-button-1 blue-bg">ارسال رمز التحقق الى البريد الالكتروني</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
+                  
                 </div>
             </div>
         </div>
