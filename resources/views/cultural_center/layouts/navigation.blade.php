9c10afac8b83bb802ecbb5e91678b09a0ee83e4e
AhmedMahmoud962 <am6944430@gmail.com>
2024-02-17 23:20:13 +0200

    update

--- resources/views/cultural_center/layouts/navigation.blade.php
+++ resources/views/cultural_center/layouts/navigation.blade.php
@@ -1,21 +1,10 @@
 <header class="header-section">
-  <!-- small header -->
-  <div class="small-header"> 
-    <nav class="samll-nav">
-                <div>
-                        <a href="/register" style="color:#fff !important ; font-weight:bold;padding:0 7px;" >
-                                <i class="fa fa-user" aria-hidden="true"></i>
-                                دخول المستفيدين
-                            </a>
-                </div>
-    </nav>
-  </div>
     <div class="header-menu">
         <div class="container">
             <nav class="navbar navbar-expand-xl btco-hover-menu">
                 <a class="navbar-brand logo" href="{{ url('/') }}">
                     <div class="nav-logo">
-                    <img src="{{asset($meta?->header_logo)}}" alt="{{$meta?->og_title}}">
+                    <img src="{{asset($meta->header_logo)}}" alt="{{$meta->og_title}}">
                     </div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navmenu"
@@ -28,7 +17,7 @@
                         <li><a class="dropdown-item" href="/office" > عن المكتب </a></li>
                         <li><a class="dropdown-item" href="/list" > اللوائح والقرارات</a></li>
                         <li><a class="dropdown-item" href="/university" > الجامعات المعتمدة</a></li>
-                        <!--<li><a class="dropdown-item" href="/electronic-gate" >البوابة الالكترونية</a></li>-->
+                        <li><a class="dropdown-item" href="/electronic-gate" >البوابة الالكترونية</a></li>
                         <li><a class="dropdown-item" href="/news" > الأخبار والفعاليات</a>
                         </li>
                         <li><a class="dropdown-item" href="/contact" > التواصل </a></li>
@@ -49,17 +38,10 @@
                                     </li>
                                 </ul>
                         @else
-                        <!-- <a href="/register" class="btn btn-button btn-button-1 popin black-bg" style="color:#fff !important" >
+                            <a href="/register" class="btn btn-button btn-button-1 popin black-bg">
                                 <i class="fa fa-user" aria-hidden="true"></i>
                                 دخول المستفيدين
-                            </a> -->
-                            <ul class="pt-3 footer-icon">
-
-                                        @foreach($socials as $social)
-                                            <li style="list-style: none;"><a target="_blank" href="{{$social->link}}"><i class="{{$social->icon}}" aria-hidden="true"></i></a></li>
-                                        @endforeach
-
-                                    </ul>
+                            </a>
                         @endif
                     </div>
                 </div>
