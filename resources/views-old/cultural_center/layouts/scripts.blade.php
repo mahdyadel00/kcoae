aa7a4beef326d81d87976bc3f57293d263b8929c
Mahdy Adel Mahdy <mahdyadel00@gmail.com>
2024-02-17 19:42:09 +0200

    update register

--- resources/views-old/cultural_center/layouts/scripts.blade.php
+++
@@ -1,7 +0,0 @@
-<script src="{{asset('assets/vendor/modernizr-3.5.0.js')}}"></script>
-<!--<script src="{{asset('assets/js/jquery-3.1.1.min.js')}}"></script>-->
-<!--<script src="{{asset('assets/js/ajax-jquary.js')}}"></script>-->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
-<script src="{{asset('assets/js/main.js')}}"></script>
-<script src="{{asset('assets/js/slider.js')}}"></script>
