da4d6797b8bf897739c9bd496a7ecafaf793505e
AhmedMahmoud962 <am6944430@gmail.com>
2024-03-02 18:31:45 +0200

    page data that contain requirments

---
+++ resources/views/cultural_center/orders/data.blade.php
@@ -0,0 +1,19 @@
+@extends('cultural_center.home')
+
+@section('cultural')
+
+<main>
+<h3 style="color:#d1b686;padding:0 10px;margin:30px 0 ;">متطلبات الحصول علي شهاده لامانع</h3>
+    <div style="padding:0 40px;">
+      <ul  class="text" style="margin:20px 0 ;padding:10px 0;">
+        <li style="padding:5px 0;font-size:18px;">شهاده الثانويه</li>
+        <li style="padding:5px 0;font-size:18px;">شهاده التأمينات الاجتماعيه</li>
+        <li style="padding:5px 0;font-size:18px;">صوره جواز السفر</li>
+        <li style="padding:5px 0;font-size:18px;">صوره البطاقه المدنيه</li>
+        <li style="padding:5px 0;font-size:18px;">التخصص المطلوب</li>
+        <li style="padding:5px 0;font-size:18px;">شهاده الميلاد</li>
+      </ul>
+    </div>
+</main>
+@endsection
+
