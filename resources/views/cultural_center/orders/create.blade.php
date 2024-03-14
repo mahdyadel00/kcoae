03675f22d264b525b01d40f41b19bbde59cdbccd
Mahdy Adel Mahdy <mahdyadel00@gmail.com>
2024-03-14 19:29:58 +0200

    update

--- resources/views/cultural_center/orders/create.blade.php
+++ resources/views/cultural_center/orders/create.blade.php
@@ -9,7 +9,7 @@
         <div class="container">
             <div class="contact-form">
                 <div class="row">
-                    <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
+                    <div class="col-xl-12 col-lg-8 col-md-8 col-sm-12 col-12">
                         <div class="comment-area mb-5">
                             <div class="comment-respond">
                                 <h2>متطلبات الحصول على شهادة لامانع</h2>
@@ -27,37 +27,37 @@
                                             @method('POST')
                                             @csrf
                                             <div class="row">
-                                                <div class="col-md-12">
+                                                <div class="col-md-6">
                                                     <div class="form-group">
                                                         <label for="name">شهادة الثانوية</label>
                                                         <input id="name" class="form-control form-mane" required="" type="file" name="high_school_certificate">
                                                     </div>
                                                 </div>
-                                                <div class="col-md-12">
+                                                <div class="col-md-6">
                                                     <div class="form-group">
                                                         <label for="email">شهادة التأمينات الإجتماعية</label>
                                                         <input id="email" class="form-control form-email" required="" type="file" name="social_security">
                                                     </div>
                                                 </div>
-                                                <div class="col-md-12">
+                                                <div class="col-md-6">
                                                     <div class="form-group">
                                                         <label for="name">صورة جواز السفر </label>
                                                         <input id="name" class="form-control form-mane" required="" type="file" name="passport">
                                                     </div>
                                                 </div>
-                                                <div class="col-md-12">
+                                                <div class="col-md-6">
                                                     <div class="form-group">
                                                         <label for="name"> صورة البطاقة المدنية</label>
                                                         <input id="name" class="form-control form-mane" required="" type="file" name="national_ID">
                                                     </div>
                                                 </div>
-                                                <div class="col-md-12">
+                                                <div class="col-md-6">
                                                     <div class="form-group">
                                                         <label for="name">التخصص المطلوب  </label>
                                                         <input id="name" class="form-control form-mane" required="" type="file" name="good_conduct_certificate">
                                                     </div>
                                                 </div>
-                                                <div class="col-md-12">
+                                                <div class="col-md-6">
                                                     <div class="form-group">
                                                         <label for="name"> شهادة الميلاد </label>
                                                         <input id="name" class="form-control form-mane" required="" type="file" name="birth_certificate">
