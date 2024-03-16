d83d6676c38ba64ecb22945ce55d77fee15a3960
Mahdy Adel Mahdy <mahdyadel00@gmail.com>
2024-03-16 19:33:29 +0200

    update style

--- resources/views/cultural_center/orders/item.blade.php
+++ resources/views/cultural_center/orders/item.blade.php
@@ -35,58 +35,84 @@
                         <table class="table align-items-right">
                             <thead class="thead-dark">
                             <tr>
-                                <th scope="col">الوثائق</th>
+                                <th scope="col" class="col-lg-6">الوثائق</th>
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <th scope="row">
-                                    <h5 class="black-c popin pb-4"> شهادة الثانوية العامة  </h5>
-                                    <div class="document-img">
-                                        <img src="/{{$order->high_school_certificate}}" alt="">
-                                    </div>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    <h5 class="black-c popin pb-4"> التامينات الاجتماعية</h5>
-                                    <div class="document-img">
-                                        <img src="/{{$order->social_security}}" alt="">
-                                    </div>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    <h5 class="black-c popin pb-4">شهادة حسن السيرة والسلوك</h5>
-                                    <div class="document-img">
-                                        <img src="/{{$order->good_conduct_certificate}}" alt="">
-                                    </div>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    <h5 class="black-c popin pb-4">جواز السفر</h5>
-                                    <div class="document-img">
-                                        <img src="/{{$order->passport}}" alt="">
-                                    </div>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    <h5 class="black-c popin pb-4"> البطاقة المدنية</h5>
-                                    <div class="document-img">
-                                        <img src="/{{$order->national_ID}}" alt="">
-                                    </div>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    <h5 class="black-c popin pb-4">شهادة الميلاد</h5>
-                                    <div class="document-img">
-                                        <img src="/{{$order->birth_certificate}}" alt="">
-                                    </div>
-                                </th>
-                            </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <h5 class="black-c popin pb-4"> شهادة الثانوية العامة  </h5>
+                                        <div class="document-img">
+                                            <a href="/{{$order->high_school_certificate}}" target="_blank">
+                                                <img src="/{{$order->high_school_certificate}}" alt="">
+                                            </a>
+                                            <a href="/{{$order->high_school_certificate}}" download>
+                                                <i class="fa fa-download" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </th>
+
+                                    <th scope="row">
+                                        <h5 class="black-c popin pb-4"> التامينات الاجتماعية</h5>
+                                        <div class="document-img">
+                                            <a href="/{{$order->social_security}}" target="_blank">
+                                                <img src="/{{$order->social_security}}" alt="">
+                                            </a>
+                                            <a href="/{{$order->social_security}}" download>
+                                                <i class="fa fa-download" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <h5 class="black-c popin pb-4">شهادة حسن السيرة والسلوك</h5>
+                                        <div class="document-img">
+                                            <a href="/{{$order->good_conduct_certificate}}" target="_blank">
+                                                <img src="/{{$order->good_conduct_certificate}}" alt="">
+                                            </a>
+                                            <a href="/{{$order->good_conduct_certificate}}" download>
+                                                <i class="fa fa-download" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </th>
+
+                                    <th scope="row">
+                                        <h5 class="black-c popin pb-4">جواز السفر</h5>
+                                        <div class="document-img">
+                                            <a href="/{{$order->passport}}" target="_blank">
+                                                <img src="/{{$order->passport}}" alt="">
+                                            </a>
+                                            <a href="/{{$order->passport}}" download>
+                                                <i class="fa fa-download" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <h5 class="black-c popin pb-4"> البطاقة المدنية</h5>
+                                        <div class="document-img">
+                                            <a href="/{{$order->national_ID}}" target="_blank">
+                                                <img src="/{{$order->national_ID}}" alt="">
+                                            </a>
+                                            <a href="/{{$order->national_ID}}" download>
+                                                <i class="fa fa-download" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </th>
+                                    <th scope="row">
+                                        <h5 class="black-c popin pb-4">شهادة الميلاد</h5>
+                                        <div class="document-img">
+                                            <a href="/{{$order->birth_certificate}}" target="_blank">
+                                                <img src="/{{$order->birth_certificate}}" alt="">
+                                            </a>
+                                            <a href="/{{$order->birth_certificate}}" download>
+                                                <i class="fa fa-download" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </th>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
