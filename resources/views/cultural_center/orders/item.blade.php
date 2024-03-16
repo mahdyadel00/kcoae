334f13f22e0ef2360623226832d1d502bc24230d
Mahdy Adel Mahdy <mahdyadel00@gmail.com>
2024-03-17 01:31:52 +0200

    update order item

--- resources/views/cultural_center/orders/item.blade.php
+++ resources/views/cultural_center/orders/item.blade.php
@@ -1,41 +1,72 @@
 @extends('cultural_center.home')
 
 @section('cultural')
-
-
-
+    <style>
+        .font {
+            font-size: 15px;
+            font-weight: bold;
+        }
+    </style>
     <div class="profile pt-5 pb-80">
         <div class="container">
             <div class="main-box">
                 <div class="row">
-                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12  col-7 align-items-center c-center">
-                        <div class="hadding-text-area mt-4">
-                            <h2 class="black-c popin small">الوثائق التي قمت بتقديمها</h2>
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mt-4 col-7 align-items-center c-center">
+
+                        <div class="hadding-text-area mt-6">
+                            <h2 class="black-c popin small">البيانات الشخصية </h2>
                         </div>
-                        <div class="col-md-6">
-                            <label> الاسم  : </label>
-                            <span>{{$order->client->name}}</span>
+
+                        <div class="col-md-12" style="display: inline;margin-left: 300px">
+                            <label class="font"> الاسم الأول  : </label>
+                            <span>{{$order->client->first_name_ar}}</span>
                         </div>
-                        <div class="col-md-6">
-                            <label> البريد الاكتروني  : </label>
+
+                        <div class="col-md-6" style="display: inline-block;margin-right: 100px">
+                            <label class="font"> الاسم الأول(إنجليزي) : </label>
+                            <span>{{$order->client->first_name_en }}</span>
+                        </div>
+
+                        <div class="col-md-6" style="display: inline;margin-left: 300px">
+                            <label class="font"> الاسم الثاني  : </label>
+                            <span>{{$order->client->second_name_ar}}</span>
+                        </div>
+
+                        <div class="col-md-6" style="display: inline-block;margin-right: 100px">
+                            <label class="font"> الاسم الثاني(إنجليزي) : </label>
+                            <span>{{$order->client->second_name_en }}</span>
+                        </div>
+
+                        <div class="col-md-6" style="display: inline;margin-left: 300px">
+                            <label class="font"> الاسم الثالث  : </label>
+                            <span>{{$order->client->third_name_ar}}</span>
+                        </div>
+
+                        <div class="col-md-6" style="display: inline-block;margin-right: 100px">
+                            <label class="font"> الاسم الثالث(إنجليزي) : </label>
+                            <span>{{$order->client->third_name_en }}</span>
+                        </div>
+
+                        <div class="col-md-6" style="display: inline;margin-left: 150px">
+                            <label class="font"> البريد الاكتروني  : </label>
                             <span>{{$order->client->email}}</span>
                         </div>
-                        <div class="col-md-6">
-                            <label> رقم الهاتف  : </label>
+                        <div class="col-md-6" style="display: inline-block;margin-right: 90px">
+                            <label class="font"> رقم الهاتف  : </label>
                             <span>{{$order->client->mobile}}</span>
                         </div>
-                        <div class="col-md-6">
-                            <label> الرقم الوطني  : </label>
+                        <div class="col-md-4" style="display: inline-block;">
+                            <label class="font"> الرقم الوطني  : </label>
                             <span>{{$order->client->ID_number}}</span>
                         </div>
-                        <div class="col-md-6">
-                            <label> تاريخ تقديم الطلب  : </label>
+                        <div class="col-md-6" style="display: inline-block;margin-right: 176px">
+                            <label class="font"> تاريخ تقديم الطلب  : </label>
                             <span>{{$order->client->created_at->toDateString()}}</span>
                         </div>
                         <table class="table align-items-right">
                             <thead class="thead-dark">
                             <tr>
-                                <th scope="col" class="col-lg-6">الوثائق</th>
+                                <th scope="col" class="col-lg-6">الوثائق التي قمت بتقديمها</th>
                             </tr>
                             </thead>
                             <tbody>
